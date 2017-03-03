3db246b2b2e0f4aac48e671cb10a92440e95f679
Fabio Oesch <fabiooesch@hotmail.com>
2017-03-03 09:21:50 +0000

    random stuff

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
 
 def main():
-    print('bla')
+    frame = simplegui.create_frame('this frame', 400, 400)
+    frame.start()
+
 
 if __name__ == "__main__":
     main()
