04311311f7ae6cefd5e70b5b4bc0f03aaae223a8
Fabio Oesch <fabiooesch@hotmail.com>
2017-03-03 09:31:45 +0000

    started with vector class

---
+++ vector.py
@@ -0,0 +1,17 @@
+import math
+
+class Vector():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def magnitude(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def norm(self):
+        return Vector(self.x / self.magnitude(), self.y / self.magnitude())
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+
